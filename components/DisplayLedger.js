20bc1782482838d0b9be8f12c1c910d52141063c
Chris Read <24418909+rcreadii@users.noreply.github.com>
2021-09-16 23:56:49 -0700

    display ledger entries IN PROGRESS

--- components/DisplayLedger.js
+++ components/DisplayLedger.js
@@ -1,12 +1,40 @@
 import React from "react";
+import { Segment } from "semantic-ui-react";
 
 export default function DisplayLedger({ ledger }) {
-  const income = [];
-  const payment = [];
-  const dailyCashRelease = [];
-  // for (entry of ledge) {}
+  const income = ledger["income"];
+  const payment = ledger["payment"];
+  const dailyCashRelease = ledger["dailyCashRelease"];
 
   console.log(ledger);
+  console.log(payment);
 
-  return <div></div>;
+  const makeRow = (entry) => {
+    return (
+      <Segment.Group horizontal>
+        <Segment>{entry.date.toLocaleTimeString()}</Segment>
+        <Segment>{entry.title}</Segment>
+        <Segment>{entry.amount}</Segment>
+        <Segment>{entry.frequency}</Segment>
+      </Segment.Group>
+    );
+  };
+
+  return (
+    <div>
+      {income.length > 0 ||
+      payment.length > 0 ||
+      dailyCashRelease.length > 0 ? (
+        <Segment.Group horizontal>
+          <Segment>Date</Segment>
+          <Segment>Category</Segment>
+          <Segment>Amount</Segment>
+          <Segment>Frequency</Segment>
+        </Segment.Group>
+      ) : null}
+      {dailyCashRelease.map((entry) => makeRow(entry))}
+      {income.map((entry) => makeRow(entry))}
+      {payment.map((entry) => makeRow(entry))}
+    </div>
+  );
 }
